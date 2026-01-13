f02f5f44771336166006cda7aefbfea1e43b5c9b
NestleeQ <tlustiyar@yandex.com>
2026-01-13 18:32:01 +0300

    feat: add a function for currency conversion

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,3 +3,11 @@ import { clsx, type ClassValue } from 'clsx'
 export function cn(...inputs: ClassValue[]) {
 	return clsx(inputs)
 }
+
+export function convertPrice(price: number): string {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2
+	}).format(price / 100)
+}
